151c91c03b230c8582a255254c15f4efdbedd367
mahmoudlotf0 <lotfymahmoud07@outlook.com>
2022-04-01 23:25:29 +0200

    finish Home Page UI

---
+++ lib/ui/screens/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:weatherapp/ui/pages/search_page.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) {
+                  return const SearchPage();
+                }),
+              );
+            },
+            icon: const Icon(Icons.search),
+          ),
+        ],
+        title: const Text('Weather App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Text(
+              'There is no weather 😔 start',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            ),
+            Text(
+              'searching now 🔍',
+              style: TextStyle(
+                fontSize: 30,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
