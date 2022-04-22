252c431e8857055e624ed4d2818ba55c9e13048c
mahmoudlotf0 <lotfymahmoud07@outlook.com>
2022-04-22 02:43:38 +0200

    Add cityName in model

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -1,23 +1,26 @@
 class WeatherModel {
   String weatherStateName;
-  String applicableDate;
+  DateTime date;
   double minTemp;
   double maxTemp;
   double theTemp;
+  String cityName;
   WeatherModel({
     required this.weatherStateName,
-    required this.applicableDate,
+    required this.date,
     required this.minTemp,
     required this.maxTemp,
     required this.theTemp,
+    required this.cityName,
   });
   factory WeatherModel.fromJson(Map<String, dynamic> data) {
     return WeatherModel(
       weatherStateName: data['weather_state_name'],
-      applicableDate: data['applicable_date'],
+      date: DateTime.parse(data['created']),
       minTemp: data['min_temp'],
       maxTemp: data['max_temp'],
       theTemp: data['the_temp'],
+      cityName: '',
     );
   }
 }
